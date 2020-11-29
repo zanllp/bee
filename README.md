dd9adfc3a8d5c9673b6b138f200fb7e6e385e5e8
root <root@DESKTOP-323356T.localdomain>
2020-11-30 00:22:17 +0800

    增加使用socket.io实现sh命令执行的输入输出

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# xxx
+# bee
+项目部署及服务器监控client，取代数据流链接原有使用的github action
---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import { exec } from 'child_process';
+import { argv } from 'process';
+import io from 'socket.io-client';
+
+interface AppArguments {
+    port?: string;
+    ip?: string;
+    name?: string;
+}
+
+const deployProject = (command: string, socket: SocketIOClient.Socket) => {
+    const child = exec(command);
+    child.stdout?.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        socket.emit('deploy-stdout', data);
+    });
+
+    child.stderr?.on('data', (data) => {
+        socket.emit('deploy-stderr', data);
+        console.error(`stderr: ${data}`);
+    });
+
+    child.on('close', (code) => {
+        socket.emit('deploy-exit', code);
+        console.log(`子进程退出，退出码 ${code}`);
+    });
+};
+
+const main = () => {
+    const args = argv
+        .filter(arg => arg.startsWith('-'))
+        .reduce<AppArguments>((p, c) => {
+            const reg = /^-(\w+)=(.+)$/.exec(c);
+            if (!reg) {
+                throw new Error(`启动参数异常: ${c}`);
+            }
+            p[reg[1] as keyof AppArguments] = reg[2];
+            return p;
+        }, {});
+    let { ip, port, name } = args;
+    ip = ip || '127.0.0.1';
+    port = port || '7001';
+    console.log(args);
+    const socket = io(`http://127.0.0.1:7001`, {
+        query: {
+            type: 'continue-deploy',
+            role: 'worker',
+            name
+        }
+    });
+    socket.on('connect', () => {
+        console.log('ciallo');
+    });
+    socket.on('deploy-git-project', (command: string) => deployProject(command, socket));
+};
+main();  
